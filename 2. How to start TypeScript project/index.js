9593d942ef005bbe46396397a6a59cebdb3546bc
holabee <function.hola.b@gmail.com>
2021-08-10 15:29:41 +0900

    [LearnTS] 라이브러리 설치와 컴파일 방법

---
+++ 2. How to start TypeScript project/index.js
@@ -0,0 +1,7 @@
+// ������(compile) : ts ������ js ���Ϸ� ��ȯ�ϴ� �۾�
+// npm i typescript -g
+// tsc index.ts
+function sum(a, b) {
+    return a + b;
+}
+sum(10, 20);
---
+++ 2. How to start TypeScript project/index.ts
@@ -0,0 +1,9 @@
+// 컴파일(compile) : ts 파일을 js 파일로 변환하는 작업
+// npm i typescript -g
+// tsc index.ts
+
+function sum(a:number, b:number):number{
+    return a + b;
+}
+
+sum(10, 20);
