85e2f5cb4cf604ac825f17e9fc90d167e040beda
holabee <function.hola.b@gmail.com>
2021-08-11 09:53:02 +0900

    [LearnTS] 문자열, 숫자, 배열 선언

---
+++ 3. Type definition & Variable/1_type-basic.ts
@@ -0,0 +1,13 @@
+// JS 문자열 선언
+// var str = 'hello';
+
+// TS 문자열 선언
+const str: string = 'hello';
+
+// TS 숫자 선언
+let num: number = 10;
+
+// TS 배열 선언
+let arr: Array<number> = [1, 2, 3] 
+let heroes: Array<string> = ['Captin America', 'Thor', 'Hulk']
+let items: number [] = [3, 4, 5]
