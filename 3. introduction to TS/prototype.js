d329de736144f185e9cd0a533872c428175dea10
holabee <function.hola.b@gmail.com>
2021-08-23 13:03:22 +0900

    [LearnTS] 자바스크립트의 프로토타입

--- 3. introduction to TS/prototype.js
+++ 3. introduction to TS/prototype.js
@@ -1,1 +1,10 @@
-var user = {};
+var user = { name: 'bee', age: 100 };
+
+// name: 'bee', age: 100 ม฿บน
+// var admin = { name: 'bee', age: 100, role: 'admin'};
+var admin = {}
+admin.__proto__ = user;
+console.log(admin.name);
+console.log(admin.age);
+admin.role = 'admin'
+console.log(admin.role);
