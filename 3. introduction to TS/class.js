2eea3e8ca0c3a1392d498a44a6eb968175f1384f
holabee <function.hola.b@gmail.com>
2021-08-23 13:34:21 +0900

    [LearnTS] 프로토 타입과 클래스와의 관계

--- 3. introduction to TS/class.js
+++ 3. introduction to TS/class.js
@@ -1,5 +1,12 @@
-// 클래스(ES6)
+// 생성자 함수 
+// function Person(name, age){
+//     this.name = name;
+//     this.age = age;
+// }
 
+// var teho = new Person('teho', 100)
+
+// 클래스(ES6)
 class Person {
     // class 로직
     constructor (name, age){ // 초기화 메서드 constructor
