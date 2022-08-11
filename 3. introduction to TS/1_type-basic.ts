13d94dbe6555b0ae8b5508e64dca4335d96ae967
holabee <function.hola.b@gmail.com>
2022-08-11 20:48:50 +0900

    :sparkles: tab 간격 변경
    git push origin

--- 3. introduction to TS/1_type-basic.ts
+++ 3. introduction to TS/1_type-basic.ts
@@ -129,13 +129,13 @@ if (isFunction(variable)) {
 // enum
 
 enum Weekdays {
-  Monday = 1,
-  Tuesday = 2,
-  Wednesday = 3,
-  Thursday = 4,
-  Friday = 5,
-  Saturday = 6,
-  Sunday = 7
+    Monday = 1,
+    Tuesday = 2,
+    Wednesday = 3,
+    Thursday = 4,
+    Friday = 5,
+    Saturday = 6,
+    Sunday = 7
 }
 
 console.log(Weekdays.Monday)         //1
